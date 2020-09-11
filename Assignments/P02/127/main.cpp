1c9179db67156f7c98e6e4eeaa71afa21973e498
Landen Jones <59664899+LandenSJones@users.noreply.github.com>
2020-09-10 19:00:54 -0500

    Update main.cpp

--- Assignments/P02/127/main.cpp
+++ Assignments/P02/127/main.cpp
@@ -6,37 +6,40 @@
 #include <string>
 using namespace std;
 vector<vector<string> > CreateGameBoard(vector<string>);
-vector<string> ReadGame(string, ifstream &);
+vector<string> ReadGame(string, ifstream&);
 bool FirstCompatable(string a, string b);
 bool SecondCompatable(string a, string b);
-void PlayGame(vector<vector<string> > &);
+void PlayGame(vector<vector<string> >&);
 bool IsEmpty(vector<string> vec);
-void Collapse(vector<vector<string> > &game, int index, int &stacks);
+void Collapse(vector<vector<string> >& game, int index, int& stacks);
 int main()
 {
     ifstream infile;
-    infile.open("input.txt");
+    //infile.open("input.txt");
     string firstCard;
-    infile >> firstCard;
+    cin >> firstCard;
 
     while (firstCard[0] != '#')
     {
         vector<string> data = ReadGame(firstCard, infile); //READS DATA FROM TEXT FILE , , infile
         vector<vector<string> > game = CreateGameBoard(data);
         PlayGame(game);
-        infile >> firstCard; //RESETS
+        cin >> firstCard; //RESETS
+        if (firstCard[0] != '#')
+            cout << endl;
     }
+    cout << '\n';
     return 0;
 }
 
-vector<string> ReadGame(string firstCard, ifstream &infile) //, ifstream &infile
+vector<string> ReadGame(string firstCard, ifstream& infile) //, ifstream &infile
 {
     vector<string> data;
     data.push_back(firstCard);
     string temp;
     for (int i = 0; i < 51; i++)
     {
-        infile >> temp;
+        cin >> temp;
         data.push_back(temp);
     }
     return data;
@@ -52,28 +55,27 @@ vector<vector<string> > CreateGameBoard(vector<string> cards)
     }
     return result;
 }
-void PlayGame(vector<vector<string> > &game)
+void PlayGame(vector<vector<string> >& game)
 {
     bool running = true;            //Condition to see if game is running
     int i = 0, stacks = 52;                      //Index variable
-
-    while(running == true)
+    while (running == true)
     {
-        if(i == 0)
+        if (i == 0)
         {
             i++;
         }
-        else if(i >= stacks)
+        else if (i >= stacks)
         {
             running = false;
         }
-        else if(i > 0 && i < 3)
+        else if (i > 0 && i < 3)
         {
-            if(FirstCompatable(game[i].back(), game[i-1].back()) || SecondCompatable(game[i].back(), game[i-1].back()))
+            if (FirstCompatable(game[i].back(), game[i - 1].back()) || SecondCompatable(game[i].back(), game[i - 1].back()))
             {
-                game[i-1].push_back(game[i].back());
+                game[i - 1].push_back(game[i].back());
                 game[i].pop_back();
-                if(game[i].empty())
+                if (game[i].empty())
                 {
                     Collapse(game, i, stacks);
                 }
@@ -81,25 +83,25 @@ void PlayGame(vector<vector<string> > &game)
             }
             else
                 i++;
-                
+
         }
-        else if(i >= 3 && i < stacks)
+        else if (i >= 3 && i < stacks)
         {
-            if(FirstCompatable(game[i].back(), game[i-3].back()) || SecondCompatable(game[i].back(), game[i-3].back()))
+            if (FirstCompatable(game[i].back(), game[i - 3].back()) || SecondCompatable(game[i].back(), game[i - 3].back()))
             {
-                game[i-3].push_back(game[i].back());
+                game[i - 3].push_back(game[i].back());
                 game[i].pop_back();
-                if(game[i].empty())
+                if (game[i].empty())
                 {
                     Collapse(game, i, stacks);
                 }
                 i = i - 3;
             }
-            else if(FirstCompatable(game[i].back(), game[i-1].back()) || SecondCompatable(game[i].back(), game[i-1].back()))
+            else if (FirstCompatable(game[i].back(), game[i - 1].back()) || SecondCompatable(game[i].back(), game[i - 1].back()))
             {
-                game[i-1].push_back(game[i].back());
+                game[i - 1].push_back(game[i].back());
                 game[i].pop_back();
-                if(game[i].empty())
+                if (game[i].empty())
                 {
                     Collapse(game, i, stacks);
                 }
@@ -114,19 +116,21 @@ void PlayGame(vector<vector<string> > &game)
             running = false;
         }
     }
-    cout << stacks << " piles remaining: ";
-    for(int j = 0; j < stacks; j++)
+    if (stacks == 1)
+        cout << stacks << " pile remaining: ";
+    else
+        cout << stacks << " piles remaining: ";
+    for (int j = 0; j < stacks; j++)
     {
-        cout << game[j].size()<< ' ';
+        cout << game[j].size() << ' ';
     }
-    cout << endl;
 }
-void Collapse(vector<vector<string> > &game, int index, int &stacks)
+void Collapse(vector<vector<string> >& game, int index, int& stacks)
 {
-    for(int i = index; i < stacks; i++)
+    for (int i = index; i < stacks - 1; i++)
     {
-        game[i] = game[i+1];
-        game[i+1].clear();
+        game[i] = game[i + 1];
+        game[i + 1].clear();
     }
     stacks--;
 }
