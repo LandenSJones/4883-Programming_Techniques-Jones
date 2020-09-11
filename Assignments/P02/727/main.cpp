bb804199a2198c58e270ea3988694d2ced937ad3
Landen Jones <59664899+LandenSJones@users.noreply.github.com>
2020-09-10 19:58:23 -0500

    Update main.cpp

--- Assignments/P02/727/main.cpp
+++ Assignments/P02/727/main.cpp
@@ -67,5 +67,5 @@ int main()
 }
 bool Precedence(char str1, char str2)
 {
-    return(str1 == '*' || str1 == '/') >= (str2 == '*' || str2 == '/');
+    return(str2 == '*' || str2 == '/')<= (str1 == '*' || str1 == '/');
 }
